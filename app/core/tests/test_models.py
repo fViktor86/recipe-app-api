ace021391ce7ce05d8e1cd2e57e2ee4545e8cc75
fViktor86 <f.viktor1986@gmail.com>
2026-02-01 16:27:10 +0100

    Fix a typo in test_models.py.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -42,7 +42,7 @@ class ModelTest(TestCase):
     def test_create_superuser(self):
         """Test creating a new superuser."""
         user = get_user_model().objects.create_superuser(
-            'test@example.com'
+            'test@example.com',
             'test123'
         )
 
